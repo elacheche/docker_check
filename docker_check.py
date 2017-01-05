0460051ea9082e3ec2cc3b5554030fc7e7a4ec45
El Acheche ANIS <elacheche@ubuntu.com>
2017-01-05 18:55:26 +0100

    Perfdata output is set

--- docker_check.py
+++ docker_check.py
@@ -44,13 +44,13 @@ def get_cpu_pct(ct):
 	# should find a way to keep ODU & OCU values : OS STATS LOGS or usa a tmp file
 	usage=str(os.popen("docker stats --no-stream=true "+ct).read()).split()
 	usage_pct=usage[usage.index(ct)+1]
-	return usage_pct[:-1]
+	return float(usage_pct[:-1])
 
 def get_net_io(ct):
 	net=client.containers.get(ct).stats(stream=False)['networks']
 	net_in=net['eth0']['rx_bytes']
 	net_out=net['eth0']['tx_bytes']
-	return net_in,net_out
+	return [net_in,net_out]
 
 def get_disk_io(ct):
 	disk=client.containers.get(ct).stats(stream=False)['blkio_stats']['io_service_bytes_recursive']
@@ -60,24 +60,19 @@ def get_disk_io(ct):
 
 
 def main():
-	#print(str(sys.argv))
-#	arg=sys.argv[1]
-	#print(arg)
-##	client=docker.from_env(version="1.21") #try except the version based on docker version | grep "server api"
-##	ls=client.containers.list()
-##	ct=[]
-##	for i in ls:
-##		ct.append(str(i).replace('<','').replace('>','').split()[1])
-	#cl=client.containers.get("3f106f3328")
+	summary = ''
+	stats = {}
 	for i in ct:
-		#print(client.containers.get(i).stats(stream=False)[arg])
-		print('{} → {}'.format(i, get_mem_pct(i)))
-		print('{} → {}'.format(i, get_cpu_pct(i)))
-		print('{} → {}'.format(i, get_net_io(i)))
-		print('{} → {}'.format(i, get_disk_io(i)))
-#		for j,k in client.containers.get(i).stats(stream=False).items():
-#			print(j)
-
+		mem_pct = get_mem_pct(i)
+		cpu_pct = get_cpu_pct(i)
+		net_in = get_net_io(i)[0]
+		net_out = get_net_io(i)[1]
+		disk_in = get_disk_io(i)[0]
+		disk_out = get_disk_io(i)[1]
+		stats[i+'_mem_pct'] = mem_pct
+		stats[i+'_cpu_pct'] = cpu_pct
+		summary += '{}_mem_pct={}% {}_cpu_pct={}% {}_net_in={} {}_net_out={} {}_disk_in={} {}_disk_out={} '.format(i, mem_pct, i, cpu_pct, i, net_in, i, net_out, i, disk_in, i, disk_out)
+	print(stats)
 '''
 used_space=os.popen("df -h / | grep -v Filesystem | awk '{print $5}'").readline().strip()
 
